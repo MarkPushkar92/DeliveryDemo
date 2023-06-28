2573ee82148da887bfb525938288a3f21d957401
Mark Pushkar <mark.pushkar92@gmail.com>
2023-06-28 12:25:15 +0700

    Initial Commit

---
+++ DeliveryDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryDemo
+//
+//  Created by Марк Пушкарь on 28.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
